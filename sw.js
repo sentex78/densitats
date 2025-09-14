3772d3ab3f4bd083bc22b3ac54ebc525f50b7904
sentex78 <salva.rmira@gmail.com>
2025-09-14 11:22:56 +0200

    backup

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('arids-v2').then(cache => cache.addAll([
-      'index.html', 'pes-a-volum.html', 'volum-a-pes.html', 'about.html',
+      'index.html', 'pes-a-volum.html', 'volum-a-pes.html', 'about.html', 'acer-corrugat.html',
       'manifest.json', 'icon-192.png', 'icon-512.png'
     ]))
   );
